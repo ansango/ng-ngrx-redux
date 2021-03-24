c70daf36232ca7fa99bf49a2b319a35d60648496
Anibal Santos <anibalsantosgo@gmail.com>
2021-03-24 17:33:38 +0100

    appstate

--- Ej_02/src/app/app.module.ts
+++ Ej_02/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { MessagesComponent } from './shared/components/messages/messages.component';
 import { InMemoryDataService } from './shared/data/in-memory-data.service';
+import { appReducer } from './store/app.state';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,10 @@ import { InMemoryDataService } from './shared/data/in-memory-data.service';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
+    }),
+    StoreModule.forRoot(appReducer),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production, // Restrict extension to log-only mode
     }),
   ],
   providers: [],
